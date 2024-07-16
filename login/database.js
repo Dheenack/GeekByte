6771ac2808d753e019020a213053a85a1ad0c341
Dheena Krishna <dheenakrishna2020@gmail.com>
2024-07-16 19:08:40 +0530

    added POST method

--- login/database.js
+++ login/database.js
@@ -1,7 +1,7 @@
 import mysql from 'mysql2';
-
+import 'body-parser';
 import  dotenv from 'dotenv';
-
+const encoder=bodyParser.urlencoded();
 dotenv.config()
 const pool = mysql.createPool({
     host: process.env.MYSQL_HOST,
@@ -17,10 +17,28 @@ const app= express();
 app.listen(8080,()=>{
     console.log("server is running on port 8080");
 })
-import path from 'path';
+import path, { parse } from 'path';
 import { fileURLToPath } from 'url';
+import bodyParser from 'body-parser';
+import { encode } from 'punycode';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 app.get("/",function(req,res){
     res.sendFile(__dirname+"/index.html");
+})
+
+app.post("/",encoder,function(req,res){
+    var username=req.body.username;
+    var password=req.body.password;
+    pool.query("SELECT * FROM login_info where username=? and user_password=?",[username,password],function(error,results,fields){
+        if(results.length>0){
+            res.redirect("/welcome");
+        }else{
+            res.redirect("/");
+        }
+        res.end()
+    })
+})
+app.get("/welcome",function(req,res){
+    res.sendFile(__dirname+"/welcome.html")
 })
